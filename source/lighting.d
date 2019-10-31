4ea7fff3b6bd98e6cfecb475b575e1e7ac8dce25
asumface <asumface@gmail.com>
2019-11-01 00:27:39 +0100

    more lighting experiments

--- source/lighting.d
+++ source/lighting.d
@@ -41,8 +41,6 @@ auto write_map(S)(S data, const(char)[] filename)
     writePng(filename.idup, data.transposed.flattened.map!((ref n) => n.v[]).joiner.array, data.length!0.to!int, data.length!1.to!int, PngType.truecolor);
 }
 
-alias light_field = Vector!(color_t, 8);
-
 size_t dir(int rot, Vector!(int, 2) v)
 {
     return dir(rot, v.v[]);
@@ -54,9 +52,9 @@ size_t dir(int rot, int[2] c...)
     out (r; r < 8)
 {
     int x = c[0] + 1;
-    int y = c[1] + 1;
+    int y = -c[1] + 1;
     uint pos = x + y * 3;
-    immutable uint[] lut = [
+    static immutable uint[] lut = [
         0, 7,   6,
         1, 255, 5,
         2, 3,   4
@@ -150,90 +148,74 @@ struct bounded_access_t(S, F)
     }
 }
 
-auto p4(Vector!(uint, 3) v)
-{
-    v *= 3;
-    v /= 10;
-    return v;
-}
-
-auto p3(Vector!(uint, 3) v)
-{
-    v *= 4;
-    v /= 10;
-    return v;
-}
-
-color_t saturate(Vector!(uint, 3) v)
-{
-    color_t result;
-    foreach (i, e; v.v[])
-        result.v[i] = cast(ubyte)(e > 255 ? 255 : e);
-    return result;
-}
+alias light_field = Slice!(ubyte*, 2, Canonical);
 
 interface light_affector
 {
-    void compute(ref in const(light_field) ingress, out light_field egress);
+    void compute(light_field ingress, light_field egress);
 }
 
 class light_source_t : light_affector
 {
-    void compute(ref in const(light_field) ingress, out light_field egress)
+    void compute(light_field ingress, light_field egress)
     {
-        foreach (i; 0 .. 8)
-            egress[i] = cast(color_t)vec3i(200, 200, 200);
+        egress[0 .. $, 0 .. $] = cast(ubyte)200;
     }
 }
 
 class air_t : light_affector
 {
-    void compute(ref in const(light_field) ingress, out light_field egress)
+    void compute(light_field ingress, light_field egress)
     {
-        light_field result;
-        result = ingress;
         //foreach (i1; 0 .. 8)
         //    foreach (i2; 0 .. 3)
         //        result[i1][i2] -= (result[i1][i2] > 0 ? 1 : 0);
-        egress = result;
+        import std.stdio;
+        import mir.utility : swap;
+        each!swap(ingress, egress);
     }
 }
 
 class vert_mirror_t : light_affector
 {
-    void compute(ref in const(light_field) ingress, out light_field egress)
+    void compute(light_field ingress, light_field egress)
     {
-        light_field r = ingress;
         import std.algorithm : swap;
-        swap(r[dir(0, -1,1)], r[dir(0,1,1)]);
-        swap(r[dir(0, -1,0)], r[dir(0,1,0)]);
-        swap(r[dir(0,-1,-1)], r[dir(0,1,-1)]);
-        r[dir(0, 0, -1)] = color_t.init;
-        r[dir(0, 0, 1)] = color_t.init;
-        egress = r;
+        egress[dir(0, -1,0), 0 .. $] = ingress[dir(0,1,0), 0 .. $];
+        egress[dir(0,-1,-1), 0 .. $] = ingress[dir(0,1,-1), 0 .. $];
+        egress[dir(0, -1,1), 0 .. $] = ingress[dir(0,1,1), 0 .. $];
+        egress[dir(0,1,0), 0 .. $] = ingress[dir(0, -1,0), 0 .. $];
+        egress[dir(0,1,-1), 0 .. $] = ingress[dir(0,-1,-1), 0 .. $];
+        egress[dir(0,1,1), 0 .. $] = ingress[dir(0, -1,1), 0 .. $];
+
+        egress[dir(0, 0, -1), 0 .. $] = cast(ubyte)0;
+        egress[dir(0, 0, 1), 0 .. $] = cast(ubyte)0;
     }
 }
 
 class hori_mirror_t : light_affector
 {
-    void compute(ref in const(light_field) ingress, out light_field egress)
+    void compute(light_field ingress, light_field egress)
     {
-        light_field r = ingress;
+        ubyte[8*3] backing_store;
+        auto r = backing_store[].sliced(ingress.lengths);
+        r[0 .. $, 0 .. $] = ingress[0 .. $, 0 .. $];
         import std.algorithm : swap;
-        swap(r[dir(0, 1,1)], r[dir(0,1,-1)]);
-        swap(r[dir(0, 0,1)], r[dir(0,0,-1)]);
-        swap(r[dir(0,-1,1)], r[dir(0,-1,-1)]);
-        r[dir(0, -1, 0)] = color_t.init;
-        r[dir(0, 1, 0)] = color_t.init;
-        egress = r;
+        each!swap(r[dir(0, 1,1), 0 .. $], r[dir(0,1,-1), 0 .. $]);
+        each!swap(r[dir(0, 0,1), 0 .. $], r[dir(0,0,-1), 0 .. $]);
+        each!swap(r[dir(0,-1,1), 0 .. $], r[dir(0,-1,-1), 0 .. $]);
+        r[dir(0, -1, 0), 0 .. $] = cast(ubyte)0;
+        r[dir(0, 1, 0), 0 .. $] = cast(ubyte)0;
+        egress[0 .. $, 0 .. $] = r[0 .. $, 0 .. $];
     }
 }
-
 void main()
 {
     auto map_data = read_map("/tmp/field.png");
-    auto ingress_data = slice!light_field(map_data.lengths);
-    auto egress_data = slice!light_field(map_data.lengths);
+    immutable size_t color_cnt = 3;
+    immutable size_t direction_cnt = 8;
+    auto ingress_data = slice!ubyte(map_data.length!0, map_data.length!1, direction_cnt, color_cnt);
+    auto egress_data = slice!ubyte(map_data.length!0, map_data.length!1, direction_cnt, color_cnt);
     auto material_table = slice!light_affector(map_data.lengths);
 
     auto air = new air_t;
@@ -242,7 +224,7 @@ void main()
     auto hmirror = new hori_mirror_t;
 
     material_table.each!((ref n) => n = air);
-    ndiota(10, 1).each!((c)
+    ndiota(1, 1).each!((c)
     {
         material_table[(vec2ul(c)+vec2ul(200,200)).v[]] = light_source;
     });
@@ -262,65 +244,87 @@ void main()
         import std.stdio;
         ndiota(map_data.lengths).each!((c) // specific egress calculation
         {
-            material_table[c].compute(ingress_data[c], egress_data[c]);
-            ingress_data[c] = light_field.init;
+            material_table[c].compute(ingress_data[c[0], c[1], 0 .. $, 0 .. $],
+                egress_data[c[0], c[1], 0 .. $, 0 .. $]);
+            ingress_data[c[0], c[1], 0 .. $, 0 .. $] = cast(ubyte)0;
         });
 
-        ndiota(map_data.lengths).each!((c) // generic ingress calculation
+        bool within_bounds(size_t[2] c...)
         {
-            foreach (d; dirs[])
+            static foreach (i; 0 .. 2)
+                if (c[i] >= map_data.length!i)
+                    return false;
+            return true;
+        }
+
+        ndiota(egress_data.lengths).each!((_input) // generic ingress calculation
+        {
+            size_t x = _input[0];
+            size_t y = _input[1];
+            int dir_e = cast(int)_input[2];
+            auto dir_v = dirs[dir_e];
+
+            int color = cast(int)_input[3];
+
+            if (!within_bounds((dir_v + vec2ul(x, y)).v[]))
+                return;
+
+            int energy = egress_data[x, y, dir_e, color];
+            while (energy > 2) // terminates after max 3 iterations
             {
-                foreach (channel; 0 .. 3)
+                //uint primary = energy * 12 / 32; // approx 1/(1/sqrt(2)+2)
+                uint primary = energy * 6_197_471 / (1u << 24); // approx 1/(1/sqrt(2)+2)
+                uint secondary = energy * 4_382_274 / (1u << 24); // approx (1/sqrt(2)+2)/sqrt(2)
+                foreach (rot; [0, -1, 1])
                 {
-                    int energy = egress_data[c][dir(0, d)][channel];
-                    while (energy > 2) // terminates after max 3 iterations (empirically tested)
+                    uint val;
+
+                    if (rot == 0)
+                        val = primary;
+                    else
+                        val = secondary;
+                    energy -= val;
+
+                    auto t_coor = (vec2ul(x, y) + dirs[dir(rot, dir_v)]).v[];
+                    uint prev_val = ingress_data[t_coor[0], t_coor[1], dir(rot, dir_v), color];
+
+                    if (cast(ubyte)(prev_val + val) < prev_val)
                     {
-                        //uint primary = energy * 12 / 32; // approx 1/(1/sqrt(2)+2)
-                        uint primary = energy * 6_197_471 / (1u << 24); // approx 1/(1/sqrt(2)+2)
-                        uint secondary = energy * 4_382_274 / (1u << 24); // approx (1/sqrt(2)+2)/sqrt(2)
-                        foreach (rot; [0, -1, 1])
-                        {
-                            uint val;
-
-                            if (rot == 0)
-                                val = primary;
-                            else
-                                val = secondary;
-                            energy -= val;
-
-                            uint prev_val = ingress_data.bounded_access(color_t.init)[(vec2ul(c) + dirs[dir(1, d)]).v[]][dir(rot, d)][channel];
-
-                            if (cast(ubyte)(prev_val + val) < prev_val)
-                            {
-                                energy += 255 - prev_val;
-                                val = 255;
-                            }
-                            else
-                                val = prev_val + val;
-                            ingress_data.bounded_access(color_t.init)[(vec2ul(c) + dirs[dir(1, d)]).v[]][dir(rot, d)][channel] = cast(ubyte)val;
-                        }
-                        assert(energy >= 0);
+                        energy += 255 - prev_val;
+                        val = 255;
                     }
-                    while (energy > 0) // distribute remaining energy randomly
-                    {
-                        import std.random;
-                        int rot = xxHash!32((*cast(ubyte[c.sizeof]*)&c)[], i) % 3 - 1;
-                        uint prev_val = ingress_data.bounded_access(color_t.init)[(vec2ul(c) + dirs[dir(1, d)]).v[]][dir(rot, d)][channel];
-                        if (prev_val < 255)
-                            prev_val += 1;
-                        ingress_data.bounded_access(color_t.init)[(vec2ul(c) + dirs[dir(1, d)]).v[]][dir(rot, d)][channel] = cast(ubyte)prev_val;
-                        energy -= 1;
-                        assert(energy >= 0);
-                    }
+                    else
+                        val = prev_val + val;
+                    ingress_data[t_coor[0], t_coor[1], dir(rot, dir_v), color] = cast(ubyte)val;
                 }
+                assert(energy >= 0);
+            }
+
+            while (energy > 0)
+            {
+                int rot = xxHash!32((cast(ubyte*)_input[].ptr)[0 .. _input.sizeof], i + energy) % 3 - 1;
+                Vector!(int, 2) shift = dirs[dir(rot, dir_v)];
+                Vector!(size_t, 2) target;
+                target.x = x;
+                target.y = y;
+                target += shift;
+                uint curr_val = ingress_data[target.x, target.y, dir(0, shift), color];
+                if (curr_val < 255)
+                {
+                    curr_val += 1;
+                    ingress_data[target.x, target.y, dir(0, shift), color] = cast(ubyte)curr_val;
+
+                }
+                energy -= 1;
             }
         });
 
         ndiota(map_data.lengths).each!((c)
         {
             import std.algorithm : dfold = fold;
-            auto light_value = ingress_data[c].dfold!((a, b) => a + b)(Vector!(uint, 3)());
-            import std.stdio;
+            uint[3] light_value;
+            auto light_slice = light_value[].sliced;
+            ingress_data[c[0], c[1], 0 .. $, 0 .. $].byDim!0.each!((v){light_slice[] += v[];});
             foreach (i; 0 .. 3)
                 map_data[c][i] = cast(ubyte)(light_value[i] > 255 ? 255 : light_value[i]);
         });
@@ -329,5 +333,18 @@ void main()
         writeln(cnt);
         write_map(map_data, format!"/tmp/result%06d.png"(cnt++));
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 ulong cnt;
